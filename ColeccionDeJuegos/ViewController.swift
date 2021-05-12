cc3cd3cb5763a23f044ebbb81558a6a37e2ebdf4
Nikols-Venegas <nikols.venegas@tecsup.edu.pe>
2021-05-12 14:46:37 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by user1 on 5/12/21.
+//  Copyright © 2021 Nikols Venegas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
